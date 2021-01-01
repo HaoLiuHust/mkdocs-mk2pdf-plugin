c686d2b18817eb0f18d05d291ffdd5fe4ca29b95
Alexandre Perrin <alexandreperr@gmail.com>
2021-01-01 17:45:39 +0000

    Removed loading requirements from pip
    PIP requirements load function is internal and does not provide a stable API. With current PIP version 20, the setup script is not working.

--- setup.py
+++ setup.py
@@ -1,24 +1,10 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
-try:
-    # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:
-    # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-from os import path as os_path
-
-this_directory = os_path.abspath(os_path.dirname(__file__))
-
-
-def load_requirements(fname):
-    reqs = parse_requirements(fname, session="test")
-    return [str(ir.req) for ir in reqs]
-
+this_directory = Path(__file__).resolve().parent
 
 def read_file(filename):
-    with open(os_path.join(this_directory, filename), encoding="utf-8") as f:
+    with open(str(this_directory / filename), encoding="utf-8") as f:
         long_description = f.read()
     return long_description
 
@@ -35,7 +21,10 @@ setup(
     author_email="alexandreperr@gmail.com",
     license="MIT",
     python_requires=">=3.4",
-    install_requires=load_requirements("requirements.txt"),
+    install_requires=[
+        "mkdocs",
+        "beautifulsoup4"
+    ],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
